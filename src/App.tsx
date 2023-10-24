f2cb8cba8eb57e1760ab7c3ad72345c2c576814f
Karthik <chivalrykarthik@gmail.com>
2023-10-24 12:53:48 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React,{ useEffect } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  
+  useEffect(()=>{
+    (async()=>{
+      const res = await axios.get('https://www.reddit.com/r/technology/hot.json');
+      console.log(res.data)
+    })()
+  },[])
+
+  return (
+    <>
+      <div>
+        Welcome
+      </div>
+    </>
+  )
+}
+
+export default React.memo(App)
