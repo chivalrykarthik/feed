1fedff855db51410312c36bb16b34399ec09209a
Karthik <chivalrykarthik@gmail.com>
2023-10-24 13:46:19 +0530

    added reddit api

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 import axios from 'axios';
 import './App.css'
 
 function App() {
-  
+  const [content,setContent] = useState([])
   useEffect(()=>{
     (async()=>{
-      const res = await axios.get('https://www.reddit.com/r/technology/hot.json');
-      console.log(res.data)
+      const {data:res} = await axios.get('https://www.reddit.com/r/technology/hot.json');
+      console.log(res?.data)
+      if(res?.data?.children){
+        console.log(res.data.children)
+        setContent(res.data.children)
+      }
     })()
   },[]);
 
   return (
     <>
-      <div>
-        Welcome
+      <div className="container">
+        <div className = "header"><h4>MyFeed</h4></div>
+      </div>
+      <div className='content'>
+        {content.map((item:any)=>{
+          const {data:{title,url}} = item;
+          return <div className="item"><a href = {url} target='__blank'>{title}</a></div>
+        })}
+        
       </div>
     </>
   )
