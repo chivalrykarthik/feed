2f33538350fa2e2fd9258b01ab76ab9e7e6a4969
Karthik <chivalrykarthik@gmail.com>
2025-06-07 18:59:37 +0530

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,13 @@ function App() {
   const [selectedTab, setSelectedTab] = useState<SelectedTab>('TECH');
   const [sortByDate, setSortByDate]= useState(false);
   const [showTop10, setShowTop10]= useState(true);
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   const [cnt, setCnt] = useState(0);
   const {content,isLoading} = useFetch(show, selectedTab);
   const handleClick = ()=>{
     const newCnt = cnt+1;
     setCnt(newCnt);
-    if(newCnt > 12)
+    if(newCnt > 5)
       setShow(true);
   }
 
